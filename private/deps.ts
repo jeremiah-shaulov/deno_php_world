b36da8c0362cc13f9ccb239a4f8095954d9852b8
Jeremiah Shaulov <jeremiah.shaulov@gmail.com>
2022-05-15 02:18:00 +0300

    Dependency updated: fcgi@v2.0.2

--- private/deps.ts
+++ private/deps.ts
@@ -1,4 +1,4 @@
-export {fcgi, ResponseWithCookies, ServerRequest} from 'https://deno.land/x/fcgi@v2.0.0/mod.ts';
+export {fcgi, ResponseWithCookies, ServerRequest} from 'https://deno.land/x/fcgi@v2.0.2/mod.ts';
 export {readAll, writeAll, copy, iterateReader} from 'https://deno.land/std@0.135.0/streams/conversion.ts';
 export {exists} from "https://deno.land/std@0.135.0/fs/mod.ts";
 export {dirname} from "https://deno.land/std@0.135.0/path/mod.ts";
