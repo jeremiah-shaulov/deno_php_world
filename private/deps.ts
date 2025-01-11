b5ed5b116c9bf8d8a09f0fd1990f7e7e12a95e75
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2025-01-11 13:47:23 +0200

    To fcgi@v2.1.0

--- private/deps.ts
+++ private/deps.ts
@@ -2,4 +2,4 @@ export * as path from 'jsr:@std/path@1.0.8';
 export {assert} from 'jsr:@std/assert@1.0.7/assert';
 export {assertEquals} from 'jsr:@std/assert@1.0.7/equals';
 export {RdStream, WrStream, TrStream} from 'https://deno.land/x/water@v1.0.26/mod.ts';
-export {fcgi, ResponseWithCookies, ServerRequest} from 'https://deno.land/x/fcgi@v2.0.8/mod.ts';
+export {fcgi, ResponseWithCookies, ServerRequest} from 'https://deno.land/x/fcgi@v2.1.0/mod.ts';
