893b4a6c818c69fc192aae22a5b0d2ceae281310
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2025-09-05 22:30:41 +0300

    Dependencies upgrade

--- private/deps.ts
+++ private/deps.ts
@@ -1,5 +1,5 @@
 export * as path from 'jsr:@std/path@1.0.8';
-export {assert} from 'jsr:@std/assert@1.0.7/assert';
-export {assertEquals} from 'jsr:@std/assert@1.0.7/equals';
+export {assert} from 'jsr:@std/assert@1.0.14/assert';
+export {assertEquals} from 'jsr:@std/assert@1.0.14/equals';
 export {RdStream, WrStream, TrStream} from 'https://deno.land/x/water@v1.0.27/mod.ts';
-export {fcgi, ResponseWithCookies, ServerRequest} from 'https://deno.land/x/fcgi@v2.1.1/mod.ts';
+export {fcgi, ResponseWithCookies, ServerRequest} from 'https://deno.land/x/fcgi@v2.1.2/mod.ts';
